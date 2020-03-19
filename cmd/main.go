a3f8b10ba8ee621afc9d1a132a66bc7d0de42ccf
Vincent Wardhana <vincentwardhana33@gmail.com>
2020-03-19 19:58:58 +0700

    move Run() func to server

--- cmd/main.go
+++ cmd/main.go
@@ -1,63 +1,7 @@
 package main
 
-import (
-	"context"
-	"flag"
-	"fmt"
-
-	_ "github.com/lib/pq"
-	"github.com/vincentwardhana33/grpc-rest-api-gokit-example/service/movie"
-
-	"github.com/go-kit/kit/log"
-
-	"github.com/go-kit/kit/log/level"
-
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-)
+import "github.com/vincentwardhana33/grpc-rest-api-gokit-example/service/movie"
 
 func main() {
-	var httpAddr = flag.String("http", ":8080", "http listen address")
-	var logger log.Logger
-	{
-		logger = log.NewLogfmtLogger(os.Stderr)
-		logger = log.NewSyncLogger(logger)
-		logger = log.With(logger,
-			"service", "movie",
-			"time:", log.DefaultTimestampUTC,
-			"caller", log.DefaultCaller,
-		)
-	}
-
-	level.Info(logger).Log("msg", "service started")
-	defer level.Info(logger).Log("msg", "service ended")
-
-	flag.Parse()
-	ctx := context.Background()
-	var srv movie.Service
-	{
-		repository := movie.NewRepo(logger)
-
-		srv = movie.NewService(repository, logger)
-	}
-
-	errs := make(chan error)
-
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
-		errs <- fmt.Errorf("%s", <-c)
-	}()
-
-	endpoints := movie.MakeEndpoints(srv)
-
-	go func() {
-		fmt.Println("listening on port", *httpAddr)
-		handler := movie.NewHTTPServer(ctx, endpoints)
-		errs <- http.ListenAndServe(*httpAddr, handler)
-	}()
-
-	level.Error(logger).Log("exit", <-errs)
+	movie.Run()
 }
--- service/movie/server.go
+++ service/movie/server.go
@@ -2,11 +2,62 @@ package movie
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/gorilla/mux"
 )
+
+func Run() {
+	var httpAddr = flag.String("http", ":8080", "http listen address")
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.NewSyncLogger(logger)
+		logger = log.With(logger,
+			"service", "movie",
+			"time:", log.DefaultTimestampUTC,
+			"caller", log.DefaultCaller,
+		)
+	}
+
+	level.Info(logger).Log("msg", "service started")
+	defer level.Info(logger).Log("msg", "service ended")
+
+	flag.Parse()
+	ctx := context.Background()
+	var srv Service
+	{
+		repository := NewRepo(logger)
+
+		srv = NewService(repository, logger)
+	}
+
+	errs := make(chan error)
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	endpoints := MakeEndpoints(srv)
+
+	go func() {
+		fmt.Println("listening on port", *httpAddr)
+		handler := NewHTTPServer(ctx, endpoints)
+		errs <- http.ListenAndServe(*httpAddr, handler)
+	}()
+
+	level.Error(logger).Log("exit", <-errs)
+}
 
 func NewHTTPServer(ctx context.Context, endpoints Endpoints) http.Handler {
 	r := mux.NewRouter()
